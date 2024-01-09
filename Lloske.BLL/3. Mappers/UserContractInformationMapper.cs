27fc329adb150103b039bfcec2b58dc37574a12f
Antoine Tilman <antoine.tilman@gmail.com>
2024-01-09 10:51:05 +0100

    Models user ok

---
+++ Lloske.BLL/3. Mappers/UserContractInformationMapper.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Models = Lloske.BLL._2._Models;
+using Entities = Lloske.DAL._2._Entities;
+
+namespace Lloske.BLL._3._Mappers
+{
+    public static class UserContractInformationMapper
+    {
+        public static Models.UserContractInformation ToModel(this Entities.UserContractInformation entity)
+        {
+            return new Models.UserContractInformation
+            {
+                Id = entity.Id,
+                Contract_type = entity.Contract_type,
+                Employment_type = entity.Employment_type,
+                Job_title = entity.Job_title,
+                Organization_entry_date = entity.Organization_entry_date,
+                Contract_start = entity.Contract_start,
+                Probation_end_date = entity.Probation_end_date,
+                Contract_end = entity.Contract_end,
+                Status = entity.Status,
+                Professional_category = entity.Professional_category,
+                Last_medical_checkup_date = entity.Last_medical_checkup_date,
+            };
+        }
+
+        public static Entities.UserContractInformation ToEntity(this Models.UserContractInformation model)
+        {
+            return new Entities.UserContractInformation
+            {
+                Id = model.Id,
+                Contract_type = model.Contract_type,
+                Employment_type = model.Employment_type,
+                Job_title = model.Job_title,
+                Organization_entry_date = model.Organization_entry_date,
+                Contract_start = model.Contract_start,
+                Probation_end_date = model.Probation_end_date,
+                Contract_end = model.Contract_end,
+                Status = model.Status,
+                Professional_category = model.Professional_category,
+                Last_medical_checkup_date = model.Last_medical_checkup_date,
+            };
+        }
+    }
+}
---
+++ Lloske.BLL/3. Mappers/UserPayrollDataMapper.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lloske.BLL._3._Mappers
+{
+    public static class UserPayrollDataMapper
+    {
+    }
+}

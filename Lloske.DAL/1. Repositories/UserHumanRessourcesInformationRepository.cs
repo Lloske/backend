caf90db60231116a93fdb60488eb561cde6dbd70
AntoTilm <antoine.tilman@gmail.com>
2024-01-10 18:09:50 +0100

    pré merge

--- Lloske.DAL/1. Repositories/UserHumanRessourcesInformationRepository.cs
+++ Lloske.DAL/1. Repositories/UserHumanRessourcesInformationRepository.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Lloske.DAL._1._1_Interfaces;
+using Lloske.DAL._2._Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,31 @@ using System.Threading.Tasks;
 
 namespace Lloske.DAL._1._Repositories
 {
-    public class UserHumanRessourcesInformationRepository
+    public class UserHumanRessourcesInformationRepository : IUserHumanRessourcesInformationRepository
     {
+        public UserHumanRessourcesInformation Create(UserHumanRessourcesInformation entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<UserHumanRessourcesInformation> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public UserHumanRessourcesInformation? GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Update(int id, UserHumanRessourcesInformation entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
